b146b27fe7a18dcabd1fea6ae6f140888438f6af
AlexxMotta <alexmota@alu.ufc.br>
2023-02-07 17:31:13 -0300

    adicionando as rotas de busca(all e unique) e update

--- src/main/java/com/example/CFY/controller/CarController.java
+++ src/main/java/com/example/CFY/controller/CarController.java
@@ -1,8 +1,13 @@
 package com.example.cfy.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,23 @@ public class CarController {
 
     @PostMapping()
     public ResponseEntity<Car> save(@RequestBody Car car){
+        car = this.service.save(car);
+        return ResponseEntity.ok().body(car);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<Car>> findAll(){
+        List<Car> cars = this.service.findAll();
+        return ResponseEntity.ok().body(cars);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Car> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(this.service.findById(id));
+    }
+
+    @PutMapping()
+    public ResponseEntity<Car> update(@RequestBody Car car){
         car = this.service.save(car);
         return ResponseEntity.ok().body(car);
     }
