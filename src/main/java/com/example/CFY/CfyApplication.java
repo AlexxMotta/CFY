b5b8b4b00761356db270fc42a5c8b04af990e0dd
AlexxMotta <alexmota@alu.ufc.br>
2023-02-07 14:48:07 -0300

    starting project

---
+++ src/main/java/com/example/CFY/CfyApplication.java
@@ -0,0 +1,13 @@
+package com.example.CFY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CFY/CfyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CFY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
